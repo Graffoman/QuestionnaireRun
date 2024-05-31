85c921f7d54548833d52ded3221563b7884d1199
Александр Солодовников <A.Solodovnikov.PGES@yandex.ru>
2024-05-31 14:20:35 +0300

    Add project files.

---
+++ Domain.Entities/Interfaces/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities.Interfaces
+{
+    public interface IEntity<TId>
+    {
+        TId Id { get; set; }
+    }
+}
---
+++ Infrastructure.EntityFramework/EntityFrameworkInstaller.cs
@@ -0,0 +1,6 @@
+﻿namespace Infrastructure.EntityFramework
+{
+    internal class EntityFrameworkInstaller
+    {
+    }
+}
